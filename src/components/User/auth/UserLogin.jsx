f5bda52dda0a8b534196bbef7e3a8e30ac15f6d9
Yash Sain <yashsain.2406@gmail.com>
2024-03-06 17:36:24 +0530

    user auth pages added

---
+++ src/components/User/auth/UserLogin.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next';
+import authBackground from '../../../assets/auth-background.jpeg'
+import Logo from '../../../assets/logo.png'
+import Button from '../../../ui/Button';
+import Input from '../../../ui/Input';
+import { Link } from 'react-router-dom';
+
+const UserLogin = () => {
+    const { t } = useTranslation();
+  return (
+    <div className='relative w-full h-[100vh] flex items-center justify-center'>
+      <img src={authBackground} className='absolute z-1 w-full h-full object-cover top-0' alt="" />
+      <div className='flex flex-col items-center bg-white lg:w-[40%] md:w-[60%] w-[95%] z-10 p-8 rounded-lg opacity-90'>
+        <img className='w-[167px]' src={Logo} alt="" />
+        <div className="flex flex-col items-center w-[100%] justify-around">
+          {/* <input type="text" placeholder='Name' className='border-2 text-[#CACACA] h-[]' /> */}
+          <form className='flex sm:flex-row w-full flex-col flex-wrap justify-around items-center gap-2' action="">
+
+            <Input placeholder={t('email')} type="email" classes="large"  />
+            <Input placeholder={t('password')} type="password" classes="large"  />
+            <Button to="/ProfessionalDashboard" type="purpleButton">{t('register')}</Button>
+          </form>
+          <div className="flex justify-between w-full mt-8">
+            <Link className='p-2 border rounded-sm' to='/userRegister'>{t('createAcc')}</Link>
+            <Link className='p-2 border rounded-sm'>{t('forgotPass')}</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserLogin
