7fb1b1543816b5c9bde6f6e40eb34da4cbed05d8
Sgvreactteam <reactsgv@gmail.com>
2024-03-16 10:47:02 +0530

    new today😎

--- src/components/Professionals/pages/ProDashboard/VenuesForm/VenuesForm.jsx
+++ src/components/Professionals/pages/ProDashboard/VenuesForm/VenuesForm.jsx
@@ -2,98 +2,155 @@ import React, { useState } from "react";
 import Input from "../../../../../ui/Input";
 import AddImages from "../../../../../ui/AddImages";
 import Button from "../../../../../ui/Button";
-import { useTranslation } from 'react-i18next';
-import "./numberForm.css"
+import { useTranslation } from "react-i18next";
+import "./numberForm.css";
+import AddressAutoComplete from "../AddressAutoComplete";
+
 const VenuesForm = () => {
   const { t } = useTranslation();
+
   const checkBoxArry = [
     {
       Heading: "Indicate whether the following criteria are fulfilled ",
       labels: [
-        { id: "1", label: t('profKitchen') },
-        { id: "1", label: t('withTerr') },
-        { id: "1", label: t('withGar') },
-        { id: "1", label: t('soundSysAva') },
-        { id: "1", label: t('lightsAva') },
-        { id: "1", label: t('microAva') },
-        { id: "1", label: t('beamer') },
-        { id: "1", label: t('danceFloor') },
-        { id: "1", label: t('skittle') },
+        { id: "1", label: t("profKitchen") },
+        { id: "1", label: t("withTerr") },
+        { id: "1", label: t("withGar") },
+        { id: "1", label: t("soundSysAva") },
+        { id: "1", label: t("lightsAva") },
+        { id: "1", label: t("microAva") },
+        { id: "1", label: t("beamer") },
+        { id: "1", label: t("danceFloor") },
+        { id: "1", label: t("skittle") },
         { id: "1", label: "Bowling Alley" },
-        { id: "1", label: t('handi') },
-        { id: "1", label: t('pets') }
+        { id: "1", label: t("handi") },
+        { id: "1", label: t("pets") },
       ],
     },
   ];
 
   return (
     <div className=" ">
-        <div className="text-[#8D303A] border-b border-gray pb-4 text-2xl font-con px-10">
-            {t('createAnn')}
-        </div>
-       <div className=" max-w-[1220px] px-10">
-        <p className='mt-8 font-con text-[18px]'>{t('venues')}</p> 
-        <form className='flex flex-wrap mt-8 font-pop' action="">
-            <div className='flex flex-col md:w-[33%] w-full'>
-                <label htmlFor="">{t('name')}</label>
-                <Input placeholder={t('name')} type="text" classes="large"></Input>
-            </div>
-            <div className='flex flex-col md:w-[33%] w-full'>
-                <label htmlFor="">{t('phoneNum')}</label>
-                <Input placeholder={t('phoneNum')} type="number" classes="large"></Input>
-            </div>
-            <div className='flex flex-col md:w-[33%] w-full'>
-                <label htmlFor="">{t('website')}</label>
-                <Input placeholder="Url..." type="website" classes="large"></Input>
-            </div>
-            <div className='flex flex-col md:w-[50%] w-full'>
-                <label htmlFor="">{t('address')}</label>
-                <Input placeholder={t('address')} type="text" classes="textarea"></Input>
-            </div>
-            <div className='flex flex-col md:w-[50%] w-full'>
-                <label htmlFor="">{t('description')}</label>
-                <Input placeholder={t('description')} type="text" classes="textarea"></Input>
-            </div>
-            <div className='flex flex-col md:w-[33%] w-full'>
-                <label htmlFor="">{t('venueSize')}</label>
-                <Input placeholder={t('venueSize')} type="number" classes="large"></Input>
-            </div>
-            <div className='flex flex-col md:w-[33%] w-full'>
-                <label htmlFor="">{t('maxCapSit')}</label>
-                <Input placeholder={t('maxCapSit')} type="number" classes="large"></Input>
-            </div>
-            <div className='flex flex-col md:w-[33%] w-full'>
-                <label htmlFor="">{t('maxCapStand')}</label>
-                <Input placeholder={t('maxCapStand')} type="number" classes="large"></Input>
-            </div>
-            <div className='flex flex-col md:w-[33%] w-full'>
-                <label htmlFor="">{t('maxPark')}</label>
-                <Input placeholder={t('maxPark')} type="number" classes="large"></Input>
-            </div>
-            <div className='flex flex-col md:w-[33%] w-full'>
-                <label htmlFor="">{t('cateringChoice')}</label>
-                <select className='p-2 border rounded-md border-borde placeholder:text-placeholderText my-2 w-[95%] outline-none' name="Catering Choice" id="">
-                    <option value="">Select</option>
-                    <option value="Lunch">{t('lunch')}</option>
-                    <option value="Dinner">{t('dinner')}</option>
-                </select>
-            </div>
-            <div className='flex flex-col md:w-[33%] w-full'>
-                <label htmlFor="">{t('kindOfCuisine')}</label>
-                <select className='p-2 border rounded-md border-borde placeholder:text-placeholderText my-2 w-[95%] outline-none' name="Catering Choice" id="">
-                    <option value="">Select</option>
-                    <option value="Dinner">Traditional</option>
-                    <option value="Lunch">Chinese</option>
-                    <option value="Dinner">Italian</option>
-                    <option value="Dinner">Mexican</option>
-                </select>
-            </div>
+      <div className="text-[#8D303A] border-b border-gray pb-4 text-2xl font-con px-10">
+        {t("createAnn")}
+      </div>
+      <div className=" max-w-[1220px] px-10">
+        <p className="mt-8 font-con text-[18px]">{t("venues")}</p>
+        <form className="flex flex-wrap mt-8 font-pop" action="">
+          <div className="flex flex-col md:w-[33%] w-full">
+            <label htmlFor="">{t("name")}</label>
+            <Input placeholder={t("name")} type="text" classes="large"></Input>
+          </div>
+          <div className="flex flex-col md:w-[33%] w-full">
+            <label htmlFor="">{t("phoneNum")}</label>
+            <Input
+              placeholder={t("phoneNum")}
+              type="number"
+              classes="large"
+            ></Input>
+          </div>
+          <div className="flex flex-col md:w-[33%] w-full">
+            <label htmlFor="">{t("website")}</label>
+            <Input placeholder="Url..." type="website" classes="large"></Input>
+          </div>
 
-            <div className="flex flex-col w-[100%]">
-                <p>{t('uploadPic')}</p>
-                <AddImages />
-            </div>
+          <div className="flex flex-col md:w-[50%] w-full">
+            <label htmlFor="">Address</label>
+            <AddressAutoComplete onSelect={(address)=>{
+              console.log(address);
+            }} />
+          </div>
+          <div className="flex flex-col md:w-[50%] w-full">
+            <label htmlFor="">{t("Description (EN)")}</label>
+            <Input
+              placeholder={t("description")}
+              type="text"
+              classes="textarea"
+            ></Input>
+          </div>
+          <div className="flex flex-col md:w-[50%] w-full">
+            <label htmlFor="">{t("Description (DE)")}</label>
+            <Input
+              placeholder={t("description")}
+              type="text"
+              classes="textarea"
+            ></Input>
+          </div>
+          <div className="flex flex-col md:w-[50%] w-full">
+            <label htmlFor="">{t("Description (FR)")}</label>
+            <Input
+              placeholder={t("description")}
+              type="text"
+              classes="textarea"
+            ></Input>
+          </div>
+          <div className="flex flex-col md:w-[33%] w-full">
+            <label htmlFor="">{t("venueSize")}</label>
+            <Input
+              placeholder={t("venueSize")}
+              type="number"
+              classes="large"
+            ></Input>
+          </div>
+          <div className="flex flex-col md:w-[33%] w-full">
+            <label htmlFor="">{t("maxCapSit")}</label>
+            <Input
+              placeholder={t("maxCapSit")}
+              type="number"
+              classes="large"
+            ></Input>
+          </div>
+          <div className="flex flex-col md:w-[33%] w-full">
+            <label htmlFor="">{t("maxCapStand")}</label>
+            <Input
+              placeholder={t("maxCapStand")}
+              type="number"
+              classes="large"
+            ></Input>
+          </div>
+          <div className="flex flex-col md:w-[33%] w-full">
+            <label htmlFor="">{t("maxPark")}</label>
+            <Input
+              placeholder={t("maxPark")}
+              type="number"
+              classes="large"
+            ></Input>
+          </div>
+          <div className="flex flex-col md:w-[33%] w-full">
+            <label htmlFor="">{t("cateringChoice")}</label>
+            <select
+              className="p-2 border rounded-md border-borde placeholder:text-placeholderText my-2 w-[95%] outline-none"
+              name="Catering Choice"
+              id=""
+            >
+              <option className=" text-borde" disabled selected value="">
+                Select
+              </option>
+              <option value="Lunch">{t("lunch")}</option>
+              <option value="Dinner">{t("dinner")}</option>
+            </select>
+          </div>
+          <div className="flex flex-col md:w-[33%] w-full">
+            <label htmlFor="">{t("kindOfCuisine")}</label>
+            <select
+              className="p-2 border rounded-md border-borde placeholder:text-placeholderText my-2 w-[95%] outline-none"
+              name="Catering Choice"
+              id=""
+            >
+              <option className=" text-borde" disabled selected value="">
+                Select
+              </option>
+              <option value="Dinner">Traditional</option>
+              <option value="Lunch">Chinese</option>
+              <option value="Dinner">Italian</option>
+              <option value="Dinner">Mexican</option>
+            </select>
+          </div>
 
+          <div className="flex flex-col w-[100%]">
+            <p>{t("uploadPic")}</p>
+            <AddImages />
+          </div>
 
           <div className="mt-5 flex flex-col gap-4">
             {checkBoxArry &&
@@ -128,11 +185,13 @@ const VenuesForm = () => {
         </form>
 
         <div className="flex justify-between w-full mt-8 mb-8">
-          <Button type="grayButton" >Back</Button>
-          <Button type="purpleButton"to="/myannouncementss">Next</Button>
+          <Button type="grayButton">Back</Button>
+          <Button type="purpleButton" to="/myannouncementss">
+            Next
+          </Button>
         </div>
       </div>
-     </div>
+    </div>
   );
 };
 
